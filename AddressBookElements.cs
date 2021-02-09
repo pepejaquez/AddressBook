b680bf89ec51f8a4ed24d380103dfb94489796f9
pepe jaquez <therealpepejaquez@gmail.com>
2021-02-09 14:57:32 -0800

    First push of source files for addressbook

---
+++ AddressBookElements.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class AddressBookElements
+    {
+        //holds address book entries
+        public List<string> addressBookEntries = new List<string>();
+
+        private string lastName;
+        private string firstName;
+        private string address;
+        private string cityStateZip;
+        private string email;
+        private string comment;
+        public string path;
+
+        //*********************************************************************************************************************************
+        //constructor
+        public AddressBookElements()
+        {
+            Console.Title = "Address Book Entries";
+            //Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+        }
+        //*********************************************************************************************************************************
+        //address methods
+        public void SetAddress(string address)
+        {
+            this.address = address;
+        }
+
+        public string GetAddress()
+        {
+            return this.address;
+        }
+
+        //cityStateZip methods
+        public void SetCityStateZip(string cityStateZip)
+        {
+            this.cityStateZip = cityStateZip;
+        }
+
+        public string GetCityStateZip()
+        {
+            return this.cityStateZip;
+        }
+
+        //comment methods
+        public void SetComment(string comment)
+        {
+            this.comment = comment;
+        }
+
+        public string GetComment()
+        {
+            return this.comment;
+        }
+
+        //email methods
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+
+        public string GetEmail()
+        {
+            return this.email;
+        }
+
+        //first name methods
+        public void SetFirstName(string firstName)
+        {
+            while (firstName.Length == 0)
+            {
+                Console.Write("You did not enter a first name!\n\nEnter the first name: ");
+                firstName = Console.ReadLine();
+            }
+            string fixName = CaseToProper(firstName);
+            this.firstName = fixName;
+        }
+
+        public string GetFirstName()
+        {
+
+            return this.firstName;
+        }
+
+        //last name methods
+        public void SetLastName(string lastName)
+        {
+            while (lastName.Length == 0)
+            {
+                Console.Write("You did not enter a last name!\n\nEnter the last name: ");
+                lastName = Console.ReadLine();
+            }
+            string fixName = CaseToProper(lastName);
+            this.lastName = fixName;
+        }
+
+        public string GetLastName()
+        {
+
+            return this.lastName;
+        }
+        //*********************************************************************************************************************************
+        //general methods
+        public string ConcatForFile()
+        {
+            string fileEntry = lastName + "|" + firstName + "|" + address + "|" + cityStateZip + "|" + email + "|" + comment;
+            return fileEntry;
+        }
+
+        public void WriteToFile(string path, List<string> input)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (string s in input)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+        }
+
+        public void ReadFromFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                //list = new string[fs.Length];
+                while (sr.EndOfStream == false)
+                {
+                    string line = sr.ReadLine();
+                    addressBookEntries.Add(line);
+                }
+            }
+        }
+
+        public string CaseToProper(string s)
+        {
+            //changes input string to proper case
+            StringBuilder sb = new StringBuilder(s);
+            sb[0] = char.ToUpper(sb[0]);
+            string word = sb.ToString();
+            return word;
+        }
+
+        public void ViewNamesOnly(List<string> entries)
+        {
+            Console.WriteLine("CURRENT CONTACT LIST\n********************");
+            int counter = 1;
+            foreach (string s in entries)
+            {
+                string[] splitter = s.Split(new Char[] { '|' });
+                Console.WriteLine("{0}) {1}, {2}", counter++, CaseToProper(splitter[0]), CaseToProper(splitter[1]));
+            }
+        }
+
+        public void ViewAllAddressInformation(int lineNumber, List<string> addressEntries)
+        {
+            string[] fields = { "    First Name: ", "     Last Name: ", "       Address: ", "City\\State\\ZIP: ", "         email: ", "       Comment: " };
+            string[] splitter = addressEntries[lineNumber].Split(new Char[] { '|' });
+            byte counter = 0;
+            Console.Clear();
+            Console.WriteLine("COMPLETE ADDRESS\n****************");
+            foreach (string s in splitter)
+            {
+                Console.WriteLine("{0}" + s, fields[counter++]);
+            }
+            Console.Write("\n(Press any key to return main menu)");
+        }
+
+        public void AddressAdder()
+        {
+            Console.Clear();
+            Console.Write("Enter the last name: ");
+            SetLastName(Console.ReadLine());
+
+            Console.Write("Enter the first name: ");
+            SetFirstName(Console.ReadLine());
+
+            Console.Write("Enter the address: ");
+            SetAddress(Console.ReadLine());
+
+            Console.Write("Enter the (city state zip): ");
+            SetCityStateZip(Console.ReadLine());
+
+            Console.Write("Enter the email: ");
+            SetEmail(Console.ReadLine());
+
+            Console.Write("Enter the comment: ");
+            SetComment(Console.ReadLine());
+
+            addressBookEntries.Add(ConcatForFile());
+
+            Console.Write("\n(Press any key to return main menu)");
+            Console.Clear();
+        }
+
+        public void promptForAddressBooks()
+        {
+            if (Directory.Exists("AddressBooks") == false)
+            {
+                Directory.CreateDirectory("AddressBooks");
+            }
+
+            string[] files = Directory.GetFiles("AddressBooks");
+
+            Console.WriteLine("You have " + files.Length + " address book(s).\n");
+
+            byte counter = 1;
+            foreach (string file in files)
+            {
+                string stripFileName = file.Replace("AddressBooks\\", "").Replace(".txt", "");
+                Console.WriteLine("{0}) " + stripFileName, counter++);
+            }
+
+            Console.Write("{0}) New", files.Length + 1);
+
+            Console.Write("\n\nPlease enter an address book number or enter {0} to create an address book: ", files.Length + 1);
+
+            int selection;
+            while (int.TryParse(Console.ReadLine(), out selection) == false || selection < 1 || selection > files.Length + 1)
+            {
+                Console.Write("You did not enter a valid entry.\nPlease enter an address book number or enter {0} to create an address book: ", files.Length + 1);
+
+            }
+
+            if (selection == files.Length + 1)
+            {
+                Console.Write("What is the name of the new address book?: ");
+                string bookName = Console.ReadLine();
+                while (bookName == "" || bookName.Length > 25)
+                {
+                    Console.Write("\nYou must enter a name for your address book that is 25 characters or less.\nWhat is the name of the new address book?: ");
+                    bookName = Console.ReadLine();
+                }
+
+                this.path = "AddressBooks\\" + bookName + ".txt";
+                StreamWriter newFile = new StreamWriter(this.path);
+                newFile.Close();
+            }
+
+            else if (File.Exists(files[Convert.ToInt32(selection) - 1]))
+            {
+                ReadFromFile(files[Convert.ToInt32(selection) - 1]);
+                this.path = files[Convert.ToInt32(selection) - 1];
+            }
+            else
+            {
+                Console.WriteLine("Sorry, book does not exist...");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookElements addressBook = new AddressBookElements();
+            bool whileSwitcher = true;
+
+            addressBook.promptForAddressBooks();
+
+            while (whileSwitcher)
+            {
+                Console.Clear();
+                Console.Write("\nCurrent Address Book: {0}\n\n1) View all names in address book\n2) Make new entries.\n3) Update address information\n4) Exit program\n\nEnter the number of what you want to do: ", addressBook.path.Replace("AddressBooks\\", "").Replace(".txt", ""));
+
+                byte userNumber;
+                while (byte.TryParse(Console.ReadLine(), out userNumber) == false || userNumber < 1 || userNumber > addressBook.addressBookEntries.Count)
+                {
+                    Console.Clear();
+                    Console.Write("\nInvalid input!\n\n1) View all names in address book\n2) Make new entries.\n3) Update address information\n4) Exit program\n\nEnter the number of what you want to do: ");
+                }
+
+                switch (userNumber)
+                {
+                    case 1:
+                        Console.Clear();
+                        if (addressBook.addressBookEntries.Count == 0)
+                        {
+                            Console.Write("There are no entries to view.\nPress any key to return to main menu.");
+                            Console.ReadKey();
+                            break;
+                        }
+                        addressBook.ViewNamesOnly(addressBook.addressBookEntries);
+                        Console.Write("\nTo view complete address information enter the line number\nor press enter to return to main menu: ");
+                        string viewNameInput = Console.ReadLine();
+                        if (viewNameInput == "")
+                        {
+
+                        }
+
+                        else
+                        {
+                            int userEntryNumber;
+                            while (int.TryParse(viewNameInput, out userEntryNumber) == false || userEntryNumber < 1 || userEntryNumber > addressBook.addressBookEntries.Count)
+                            {
+                                Console.Write("\nYou did not enter a valid entry.\nTo view complete address information enter the line number: ");
+                                viewNameInput = Console.ReadLine();
+                                //while (int.TryParse(Console.ReadLine(), out userEntryNumber) == false)
+                                //{
+                                //    break;
+                                //}
+
+                            }
+                            addressBook.ViewAllAddressInformation(userEntryNumber - 1, addressBook.addressBookEntries);
+                            Console.ReadKey();
+                        }
+
+                        break;
+
+                    case 2:
+                        addressBook.AddressAdder();
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        if (addressBook.addressBookEntries.Count == 0)
+                        {
+                            Console.Write("There are no entries to view.\nPress any key to return to main menu.");
+                            Console.ReadKey();
+                            break;
+                        }
+                        addressBook.ViewNamesOnly(addressBook.addressBookEntries);
+                        Console.Write("\nEnter line number to change persons information\nor press enter to return to main menu: ");
+
+                        string userInput = Console.ReadLine();
+                        if (userInput == "")
+                        {
+                        }
+                        else
+                        {
+                            int entryToChangeNumber = Int32.Parse(userInput);
+                            while (entryToChangeNumber < 1 || entryToChangeNumber > addressBook.addressBookEntries.Count)
+                            {
+                                Console.Write("\nYou did not enter a valid entry.\nEnter line number to change persons information: ");
+
+                                while (int.TryParse(Console.ReadLine(), out entryToChangeNumber) == false)
+                                {
+                                    break;
+                                }
+                            }
+                            addressBook.AddressAdder();
+                            addressBook.addressBookEntries.RemoveAt(entryToChangeNumber - 1);
+                        }
+
+                        break;
+
+                    case 4:
+                        whileSwitcher = false;
+                        break;
+                }
+
+                addressBook.WriteToFile(addressBook.path, addressBook.addressBookEntries);
+
+            }
+        }
+    }
+}
